db0404d03164010fe1d74e56fe9c7a1566731d7f
WenQing Zhuang <wqzhuang@WenQings-Air.home>
2017-02-26 16:25:09 -0500

    binary.py

--- binary.py
+++ binary.py
@@ -11,7 +11,7 @@ def zbits(n, k):
     The function takes two arguments n and k and prints all binary strings of length n that contain k zero bits
     '''
     
-    binary_list = ''.join(str(i) for i in list(it.repeat(1,(n-k))) + list(it.repeat(0,k))) # create a binary list that consists of strings 1 and 0 
+    binary_list = ''.join(list(it.repeat('1',(n-k))) + list(it.repeat('0',k))) # create a binary list that consists of strings 1 and 0 
     permutations = list(it.permutations(binary_list, n)) # generate the permutation 
-    reformat_permutations = [''.join(i for i in j) for j in permutations] # change format of permutation, for example change '1','1','0' to '110'
+    reformat_permutations = [''.join(list(i)) for i in permutations] # change format of permutation, for example change '1','1','0' to '110'
     return set(reformat_permutations) # use set to return unique results
